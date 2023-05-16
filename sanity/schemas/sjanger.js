d0a2cc47d725360e0b0c3d00b0822c9227c41e2b
Adyan <adyan850@gmail.com>
2023-05-16 20:59:44 +0200

    Liten endring

---
+++ sanity/schemas/sjanger.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'sjanger',
+    title: 'Sjanger',
+    type: 'document',
+    fields: [
+      {
+        name: 'tittel',
+        title: 'Tittel',
+        type: 'string',
+      },
+    ],
+  };
+  
