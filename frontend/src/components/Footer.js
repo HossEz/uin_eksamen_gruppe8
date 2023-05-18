a411986c148e289ea8d8bbe76e61f283f074dcc1
zurkany <111972115+zurkany@users.noreply.github.com>
2023-05-18 20:39:30 +0200

    endringer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='footer-container'>
+      <p>&copy; 2023 GameHub. Kredit til <a href='https://rawg.io/' target="_blank" rel="noreferrer">Rawg API</a>.</p>
+    </footer>
+  );
+};
+
+export default Footer;
